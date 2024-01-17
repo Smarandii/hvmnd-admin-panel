7df4083325fee2fad374d349290565002798180e
Смаранди Олег <Oleg Smarandi>
2024-01-17 23:27:04 +0300

    HVM-4 | Create a login page and auth process

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 from dotenv import \
     load_dotenv
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_pymongo import PyMongo
 load_dotenv()
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
     try:
         pipeline = [
             {"$group": {
@@ -37,8 +41,31 @@ def index():
     return render_template('users.html', users=users, totals=totals)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == os.getenv("ADMIN_LOGIN") and password == os.getenv("ADMIN_PASSWORD"):
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid Credentials. Please try again.')
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))
+
+
 @app.route('/update_balance/<user_telegram_id>', methods=['POST'])
 def update_balance(user_telegram_id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
     new_balance = request.form['balance']
     mongo.db.user_sessions.update_one({"user_telegram_id": int(user_telegram_id)},
                                       {"$set": {"balance": float(new_balance)}})
@@ -48,6 +75,8 @@ def update_balance(user_telegram_id):
 
 @app.route('/update_bonus/<user_telegram_id>', methods=['POST'])
 def update_bonus(user_telegram_id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
     new_bonus = request.form['bonus']
     mongo.db.user_sessions.update_one({"user_telegram_id": int(user_telegram_id)},
                                       {"$set": {"bonus": float(new_bonus)}})
